c448b454f764b3b2b5ea91085023743600f3bb40
Vehikl Team <go@vehikl.com>
2019-11-27 20:59:49 -0500

    remove links from welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,17 +86,6 @@
                 It has been {{ config('accident.days_since_last') }} days since our last accident.
             </div>
         </div>
-
-        <div class="links">
-            <a href="https://laravel.com/docs">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://vapor.laravel.com">Vapor</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
-        </div>
     </div>
 </div>
 </body>
